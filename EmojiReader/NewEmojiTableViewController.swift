6927606ea80f108a2543ba27c08996522dabe6dc
Kirill Suvorov <kirill_suvorov1@icloud.com>
2024-08-19 15:33:40 +0300

    added the ability to add objects

---
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -0,0 +1,50 @@
+//
+//  NewEmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by Kirill Suvorov on 19.08.2024.
+//
+
+import UIKit
+
+class NewEmojiTableViewController: UITableViewController {
+
+    var emoji = Emoji(emoji: "", name: "", description: "", isFavourite: false)
+    
+    @IBOutlet weak var emojiTF: UITextField!
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var descriptionTF: UITextField!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        updateSaveButtonState()
+    }
+
+    private func updateSaveButtonState() {
+        let emojiText = emojiTF.text ?? ""
+        let nameText = nameTF.text ?? ""
+        let descriptionText = descriptionTF.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
+    }
+    
+    @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    @IBAction func cancelButton(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTF.text ?? ""
+        let name = nameTF.text ?? ""
+        let description = descriptionTF.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, description: description, isFavourite: self.emoji.isFavourite)
+    }
+}
